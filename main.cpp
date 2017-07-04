06396eebbbf1780b3e21cac9319c266ec4e887cd
Jelmer van Amen <Jelmer.van.Amen@crv4all.com>
2017-07-04 15:40:42 +0200

    Dereference pointer

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
     time_t now = time(0);
-    tm *ltm = localtime(&now);
-    int seconds = 60 - ltm->tm_sec;
-    int minutes = 60 - 1 - ltm->tm_min;
-    int hours = 16 - 1 - ltm->tm_hour;
+    tm ltm = *localtime(&now);
+    int seconds = 60 - ltm.tm_sec;
+    int minutes = 60 - 1 - ltm.tm_min;
+    int hours = 16 - 1 - ltm.tm_hour;
     if (hours < 0) {
         cout << "You are already " << abs(++hours) << " :hours" << minutes << " :minutes, " << seconds
              << " :seconds past time" << endl;
